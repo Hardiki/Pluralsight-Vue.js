adb896cfcf73c29a36a799f5cc6c669be83ee937
techgirlhardiki <info@techgirlhardiki.info>
2018-02-12 20:34:12 +0530

    basics of JS

---
+++ basics.js
@@ -0,0 +1,29 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Ctrl+R),
+ * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
+ * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
+ */
+
+"hello" + "world";
+/*
+helloworld
+*/
+4 < 2;
+typeof(4<2);
+
+3-"6"
+/*
+boolean
+*/
+/*
+36
+*/
+/*
+-3
+*/
+/*
+undefined
+*/
