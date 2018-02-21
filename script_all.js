9f5a1e099a3cfbcceed920ca0d61bbce78b04c3a
Hardiki <hardidevre2407@gmail.com>
2018-02-22 00:18:32 +0530

    second commit

---
+++ script_all.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+	el:'#app',
+	data:{
+		message:'Hello Vue!'
+	}	
+})
+
+
+var app = new Vue({
+	el:'#app-2',
+	data:{
+		message:'Hi! ' + ' '+ new Date().toLocaleString()
+	}	
+})
+
+
